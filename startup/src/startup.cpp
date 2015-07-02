e52cbe38ae6fd73f47ac7af7c9f76147fecaf213
Alexander Infantino <infantinoalex@gmail.com>
2015-07-02 15:09:59 -0400

    Started working on file the gets the nao into a startup position

---
+++ startup/src/startup.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+#include "nao_msgs/JointAnglesWithSpeed.h"
+
+void callback(const sensor_msgs::JointState::ConstPtr& Joints){
+	
+}
+
+int main(int argc, char ** argv){
+
+	return 0;
+}
