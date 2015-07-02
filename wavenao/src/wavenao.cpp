d7f95591c2145ffcf0bfd64a9eac49a6e56f5c37
Alexander Infantino <infantinoalex@gmail.com>
2015-07-02 13:43:24 -0400

    Robot can say he is going to wave and wave smoothly when he does it

--- wavenao/src/wavenao.cpp
+++ wavenao/src/wavenao.cpp
@@ -7,6 +7,8 @@ int main(int argc, char ** argv){
 	ros::NodeHandle n;
 	ros::Rate loop_rate(10);
 
+	int i;
+
 	ros::Publisher speak = n.advertise<std_msgs::String>("/speech", 100);
 	ros::Publisher move = n.advertise<nao_msgs::JointAnglesWithSpeed>("/joint_angles", 100);
 
@@ -28,30 +30,65 @@ int main(int argc, char ** argv){
 		speak.publish(line);	
 		ros::Duration(1).sleep();
 
-		line.data = "The first thing you want to do is move your arm up over your head"
+		line.data = "The first thing you want to do is move your arm up over your head";
 		speak.publish(line);
 		ros::Duration(3).sleep();
 	
 		mrsr.joint_names.push_back("RShoulderRoll");
 		mrsp.joint_names.push_back("RShoulderPitch");
 		mrer.joint_names.push_back("RElbowRoll");
-		mrsr.joint_angles.push_back(1);
+		mrsr.joint_angles.push_back(0.3142);
 		mrsp.joint_angles.push_back(-1);
-		mrer.joint_angles.push_back( -0.0349);
-		mrsr.speed = 0.1;
-		mrsp.speed = 0.1;
-		mrer.speed = 0.1;
+		mrer.joint_angles.push_back( 0.0349);
+		mrsr.speed = 0.7;
+		mrsp.speed = 0.7;
+		mrer.speed = 0.7;
 		move.publish(mrsr);
 		move.publish(mrsp);
 		move.publish(mrer);
-		ros::Duration(1).sleep();
-	
-		//line.data = "It looks like I can only move my head though";
-		//speak.publish(line);
 		ros::Duration(2).sleep();
-		//line.data = "Looks like youll have to try again later when I write this code";
-		//speak.publish(line);
-		ros::Duration(3).sleep();
+	
+		line.data = "Just like that";
+		speak.publish(line);
+		ros::Duration(1).sleep();
+
+		line.data = "Now what you want to do it move your whole arm left and right like this";
+		speak.publish(line);
+		ros::Duration(2).sleep();
+		for(i = 0; i < 5; i++){
+			ROS_INFO("Waving\n");
+			loop_rate.sleep();
+			mrsr.joint_angles[0] = -1;
+			mrer.joint_angles[0] = 0.5;
+			mrsr.speed = 0.5;
+			mrer.speed = 0.5;
+			move.publish(mrsr);
+			move.publish(mrer);
+			ros::Duration(0.5).sleep();
+
+                        mrsr.joint_angles[0] = 0.342;
+                        mrer.joint_angles[0] = 0.349;
+                        mrsr.speed = 0.5;
+                        mrer.speed = 0.5;
+                        move.publish(mrsr);
+                        move.publish(mrer);
+                        ros::Duration(0.5).sleep();
+		}
+
+		mrsp.joint_angles[0] = 1.4;
+		mrsp.speed = 0.5;
+		move.publish(mrsp);
+		ros::Duration(1).sleep();
+
+		line.data = "Dont forget to put your arm back down";
+		speak.publish(line);
+		ros::Duration(2).sleep();
+		line.data = "Looks like we did it";
+		speak.publish(line);
+		ros::Duration(1).sleep();
+		line.data = "YEA";
+		speak.publish(line);
+		ros::Duration(20).sleep();
 	}		
 
 	return 0;
