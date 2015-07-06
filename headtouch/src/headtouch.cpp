6dc4c1dadbee8a4844a1f6d497f3489edaa10a04
Alexander Infantino <infantinoalex@gmail.com>
2015-07-06 12:46:44 -0400

    commit

---
+++ headtouch/src/headtouch.cpp
@@ -0,0 +1,13 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "nao_msgs/TactileTouch.h"
+
+int buttonf, buttonm, buttonb, buttonfp, buttonmp, buttonbp;
+
+void callback(const nao_msgs::TactileTouch::ContsPtr& Button){
+}
+
+int main(int argc, char ** argv){
+
+	return 0;
+}
