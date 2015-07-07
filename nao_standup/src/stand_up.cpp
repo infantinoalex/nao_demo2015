5f5f74cfeafe84e7fd700e1adc7117550205eb19
Victoria <Victoria_Albanese@student.uml.edu>
2015-07-07 15:54:51 -0400

    bend only hip with more force dammit

--- nao_standup/src/stand_up.cpp
+++ nao_standup/src/stand_up.cpp
@@ -556,12 +556,12 @@ int main(int argc, char **argv) {
     //narration.data = "Trying to get up.";
     pub_narration.publish(narration);
 
-    lhp.joint_angles[0] = -0.8;
-    lhp.speed = 0.01;
- 
+    lhp.joint_angles[0] = -0.9;
+    lhp.speed = 0.5;
+    /* 
     lap.joint_angles[0] = -0.4;
     lap.speed = 0.01;
-    
+    */
     pub_move.publish(lap);
     pub_move.publish(lhp);
 
