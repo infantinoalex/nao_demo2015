f44786d5992c706aff9f037cac079318340b2a66
Alexander Infantino <infantinoalex@gmail.com>
2015-07-10 09:17:42 -0400

    tried added bumper status in so nao would stop if hit anything, but broke my spaghetti code

--- programs/src/walk_detect.cpp
+++ programs/src/walk_detect.cpp
@@ -1,14 +1,22 @@
+/**
+Created by: Alexander Infantino
+**/
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Range.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Bool.h"
+#include "nao_msgs/Bumper.h"
 
 // Global variables to store sonar information
 float rsonarr, lsonarr;
 
 // Global variable to store onground bool
 bool onground = true;
+
+// Global variable to store the data for the bumper states
+int bumperp, bumpers;
 
 // Left sonar callback function to update sonar data 
 void sonarleftcb(const sensor_msgs::Range::ConstPtr& LeftSonar){
@@ -23,6 +31,12 @@ void sonarrightcb(const sensor_msgs::Range::ConstPtr& RightSonar){
 // Foot contact callback function to determine if robot is standing up
 void footcb(const std_msgs::Bool Bools){
 	onground = Bools.data;
+}
+
+// Bumper contact callback function to determine if the robot's feet have hit anything
+void bumpercb(const nao_msgs::Bumper::ConstPtr& Bump){
+	bumperp = Bump->bumper;
+	bumpers = Bump->state;
 }
 
 /* This program makes the nao walk
@@ -40,6 +54,9 @@ int main(int argc, char ** argv){
 
 	// subscribes too footcontact so that it can tell if it has fallen down or not
 	ros::Subscriber sub_2 = n.subscribe("/foot_contact", 100, footcb);
+
+	// subscribes to bumpers so that it can determine if the nao's feet have hit anything
+	ros::Subscriber sub_3 = n.subscribe("/bumper", 100, bumpercb);
 	
 	// publishers to make the nao talk/move
 	ros::Publisher talk = n.advertise<std_msgs::String>("/speech", 100);
@@ -68,31 +85,58 @@ int main(int argc, char ** argv){
 		// if nothing is too close to the nao, it will just move forward
 		if(!onground){
 			ROS_INFO("ROBOT IS NOT ON GROUND");
-			ROS_INFO("SHUTTING DOWN NODE\n");
-			words.data = "I HAVE FALLEN DOWN!";
+			//ROS_INFO("SHUTTING DOWN NODE\n");
+			words.data = "WAITING UNTIL BACK ON GROUND.";
 			talk.publish(words);
 			ros::Duration(2).sleep();
-			words.data = "SHUTTIN DOWN";
+			words.data = "WAITING.";
 			talk.publish(words);
 			ros::Duration(1).sleep();
 			loop_rate.sleep();
-			ROS_INFO("5");
+			while(!onground){
+				ros::spinOnce();
+				loop_rate.sleep();
+			}
+			ROS_INFO("BACK ON GROUND\n");
+			words.data = "RESUMING.";
+			talk.publish(words);
 			ros::Duration(1).sleep();
-			ROS_INFO("4");
-			ros::Duration(1).sleep();
-			ROS_INFO("3");
-			ros::Duration(1).sleep();
-			ROS_INFO("2");
-			ros::Duration(1).sleep();
-			ROS_INFO("1");
-			ros::Duration(1).sleep();
-			ROS_INFO("GOODBYE\n");
-			ros::Duration(1).sleep();
-			ros::shutdown();
+			loop_rate.sleep();
+			ros::spinOnce();
+			//ROS_INFO("5");
+			//ros::Duration(1).sleep();
+			//ROS_INFO("4");
+			//ros::Duration(1).sleep();
+			//ROS_INFO("3");
+			//ros::Duration(1).sleep();
+			//ROS_INFO("2");
+			//ros::Duration(1).sleep();
+			//ROS_INFO("1");
+			//ros::Duration(1).sleep();
+			//ROS_INFO("GOODBYE\n");
+			//ros::Duration(1).sleep();
+			//ros::shutdown();
 		}
+		else if((bumperp == 0 || bumperp == 1) && bumpers == 1){
+			// if bumper is hit, make the nao move backwards
+			ROS_INFO("BUMPER HIT: BACKING UP\n");
+			ros::spinOnce();
+			loop_rate.sleep();
+			move.publish(stop);
+			ros::Duration(1).sleep();
+			loop_rate.sleep();
+			direct.linear.x = -0.5;
+			direct.angular.z = 0;
+			move.publish(direct);
+			loop_rate.sleep();
+			ros::Duration(2).sleep();
+			loop_rate.sleep();
+			ros::spinOnce();
+		}	
 		else{
+			// if its clearn infront, the nao will move forward
 			if(rsonarr >= 0.32 && lsonarr >= 0.32){
-				while(rsonarr >= 0.32 && lsonarr >= 0.32){
+				while(rsonarr >= 0.32 && lsonarr >= 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 					// if statement so that ROS_INFO will not be constantly printed out
 					if(i == 0){
 						ROS_INFO("MOVING STRAIGHT\n");
@@ -112,7 +156,7 @@ int main(int argc, char ** argv){
 			}
 			// if an object is too close to the right side, the nao will turn left
 			else if(rsonarr < 0.32 && lsonarr > 0.32){
-				while(rsonarr < 0.32 && lsonarr > 0.32){
+				while(rsonarr < 0.32 && lsonarr > 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 					// if statement so that ROS_INFO will not be constantly printed out
 					if(i == 0){
 						ROS_INFO("RIGHT SIDE TO CLOSE");
@@ -127,7 +171,7 @@ int main(int argc, char ** argv){
 					}
 					else{
 						if(rsonarr < 0.32 && lsonarr < 0.32){
-							while(rsonarr < 0.32 && lsonarr < 0.32){
+							while(rsonarr < 0.32 && lsonarr < 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 								ROS_INFO("TOO CLOSE");
 								ROS_INFO("BACKING UP\n");
 								loop_rate.sleep();
@@ -146,7 +190,7 @@ int main(int argc, char ** argv){
 			}
 			// if an object is too close to the left side, the nao will turn right
 			else if(rsonarr > 0.32 && lsonarr < 0.32){
-				while(rsonarr > 0.32 && lsonarr < 0.32){
+				while(rsonarr > 0.32 && lsonarr < 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 					// if statement so that ROS_INFO will not be constantly printed out
 					if(i == 0){
 						ROS_INFO("LEFT SIDE TOO CLOSE");
@@ -161,7 +205,7 @@ int main(int argc, char ** argv){
 					}
 					else{
 						if(rsonarr < 0.32 && lsonarr < 0.32){
-							while(rsonarr < 0.32 && lsonarr < 0.32){
+							while(rsonarr < 0.32 && lsonarr < 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 								ROS_INFO("TOO CLOSE");
 								ROS_INFO("BACKING UP\n");
 								loop_rate.sleep();
@@ -180,7 +224,7 @@ int main(int argc, char ** argv){
 			}
 			// if an object is too close, the nao will back up
 			else{
-				while(rsonarr < 0.32 && lsonarr < 0.32){
+				while(rsonarr < 0.32 && lsonarr < 0.32){ //|| !((bumperp == 0 || bumperp == 1) && bumpers == 1)){
 					// if statement so that ROS_INFO will not be constantly printed out
 					if(i == 0){
 						ROS_INFO("TOO CLOSE");
