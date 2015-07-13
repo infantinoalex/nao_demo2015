458a501190a373ccf34271d135adc6f398d49533
Alexander Infantino <infantinoalex@gmail.com>
2015-07-13 13:30:15 -0400

    fixed

--- naopositions/nao_standup_facedown/src/stand_up.cpp
+++ naopositions/nao_standup_facedown/src/stand_up.cpp
@@ -13,7 +13,7 @@ void controlcb(const statepublish::states States){
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "move_robot");
+  ros::init(argc, argv, "set_pose");
   ros::NodeHandle node;
 
   //All the publishers
