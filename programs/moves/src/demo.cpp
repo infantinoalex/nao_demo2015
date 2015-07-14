269fd30f1250df7f017c78ee3da0b8f69c90f4e8
Alexander Infantino <infantinoalex@gmail.com>
2015-07-14 14:38:49 -0400

    Worked on demo

--- programs/moves/src/demo.cpp
+++ programs/moves/src/demo.cpp
@@ -25,6 +25,26 @@ int main(int argc, char ** argv){
 
 	// publoshers to make the nao talk/move
 	ros::Publisher move = n.advertise<nao_msgs::JointAnglesWithSpeed>("/joint_angles", 100);
-	
+	ros::Publisher talk = n.advertise<std_msgs::String>("/speech", 100);
+
+	// variable declarations
+	std_msgs::String words;
+	nao_msgs::JointAnglesWithSpeed mhp, mhy, mler, mrer, mley, mrey, mlwy, mrwy, mrsr, mlsr, mrsp, mlsp, mlh, mrh;
+	int speed;
+
+	ROS_INFO("SETTING JOINT STATES\n");
+	mhp.joint_names.pushback("HeadPitch");
+	mhy.joint_names.pushback("HeadYaw");	
+	mler.joint_names.pushback("LElbowRoll");
+	mrer.joint_names.pushbacl("RElbowRoll");
+	mlwy.joint_names.pushback("LWristYaw");
+	mrwy.joint_names.pushback("RWristYaw");
+	mley.joint_names.pushback("LElbowYaw");
+	mrey.joint_names.pushback("RElbowYaw");
+	mlsr.joint_names.pushback("LShoulderRoll");
+	mrsr.joint_names.pushback("RShoulderRoll");
+	mlsp.joint_names.pushback("LShoulderPitch");
+	mrsp.joint_names.pushback("RShoulderPitch");
+
 	return 0;
 }
