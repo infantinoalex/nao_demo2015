d4a0e803a3842b130dc981afc74e0695b36c41a7
Victoria <Victoria_Albanese@student.uml.edu>
2015-07-21 12:04:43 -0400

    sped up stuff

--- naopositions/nao_standup_facedown/src/stand_up.cpp
+++ naopositions/nao_standup_facedown/src/stand_up.cpp
@@ -568,29 +568,29 @@ int main(int argc, char **argv) {
 			
     			lhyp.joint_angles[0] = 0.0;
     			rhyp.joint_angles[0] = 0.0;
-    			lhyp.speed = 0.144;//0.183;
-    			rhyp.speed = 0.144;//0.183;
+    			lhyp.speed = 0.183;
+    			rhyp.speed = 0.183;
     			pub_move.publish(lhyp);
     			pub_move.publish(rhyp);
 			
     			lhr.joint_angles[0] = 0.0;
     			rhr.joint_angles[0] = 0.0;
-    			lhr.speed = 0.05;//0.063;
-    			rhr.speed = 0.05;//0.063;
+    			lhr.speed = 0.063;
+    			rhr.speed = 0.063;
     			pub_move.publish(lhr);
     			pub_move.publish(rhr);
 			
     			lhp.joint_angles[0] = -0.7;
     			rhp.joint_angles[0] = -0.7;
-    			lhp.speed = 0.117;//0.149;
-    			rhp.speed = 0.117;//0.149;
+    			lhp.speed = 0.149;
+    			rhp.speed = 0.149;
     			pub_move.publish(lhp);
     			pub_move.publish(rhp);
 
     			lap.joint_angles[0] = -1.2;
     			rap.joint_angles[0] = -1.2;
-    			lap.speed = 0.105;//0.134;
-    			rap.speed = 0.105;//0.134;
+    			lap.speed = 0.134;
+    			rap.speed = 0.134;
     			pub_move.publish(lap);
     			pub_move.publish(rap);
 			
