d680fa469d1c27f2c8897c2c070ae20ad3455ea3
Alexander Infantino <infantinoalex@gmail.com>
2015-07-21 12:13:59 -0400

    fix

--- programs/vision/src/facedetect.cpp
+++ programs/vision/src/facedetect.cpp
@@ -7,10 +7,6 @@
 #include "opencv2/objdetect/objdetect.hpp"
 
 static const std::string OPENCV_WINDOW = "Image window";
-String face_cascade_name = "haarcascade_frontalface_alt.xml";
-String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
 
 class ImageConverter
 {
@@ -43,34 +39,18 @@ class ImageConverter
 			ROS_ERROR("cv_bridge exception: %s", e.what());
 			return;
 		}
-		std::vector<Rect> faces;
-  		Mat frame_gray;
-
-  		cvtColor( frame, frame_gray, CV_BGR2GRAY );
-  		equalizeHist( frame_gray, frame_gray );
-
-  		//-- Detect faces
-  		face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-  		for( size_t i = 0; i < faces.size(); i++ ){
-  		 	Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    			ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-    			Mat faceROI = frame_gray( faces[i] );
-    			std::vector<Rect> eyes;
-
-    			//-- In each face, detect eyes
-    			eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-
-    			for( size_t j = 0; j < eyes.size(); j++ ){
-       				Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
-       				int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-       				cv::circle(cv_ptr->image, cv::Point(center), radius, CV_RGB(255,0,0))
-     			}
-  		}
 		// Update GUI Window
    		cv::imshow(OPENCV_WINDOW, cv_ptr->image);
       	 	cv::waitKey(3);
        
        		// Output modified video stream
        		image_pub_.publish(cv_ptr->toImageMsg());
+	}
+};
+int main(int argc, char** argv)
+{
+  	ros::init(argc, argv, "image_converter");
+  	ImageConverter ic;
+  	ros::spin();
+  	return 0;
+}
