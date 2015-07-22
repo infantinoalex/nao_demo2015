b299a4ab4346886944423495e052cf4cdee3468c
Alexander Infantino <infantinoalex@gmail.com>
2015-07-22 11:13:15 -0400

    fix

--- programs/functions/src/rollercoaster.cpp
+++ programs/functions/src/rollercoaster.cpp
@@ -2,7 +2,7 @@
 #include "std_msgs/String.h"
 
 int main(int argc, char ** argv){
-        ros::init(argc, argv, "Lilo&Stich");
+        ros::init(argc, argv, "LiloStich");
         ros::NodeHandle n;
         ros::Rate loop_rate(10);
 
@@ -10,9 +10,21 @@ int main(int argc, char ** argv){
 
         std_msgs::String words;
 
-	words.data = "Aloha ē, aloha ē .Anoʻai ke aloha ē Aloha ē, aloha ē ʻAnoʻai ke aloha ē. . . There’s no place I’d rather be. . . Than on my surfboard out at sea. . . Lingering in the ocean blue. . . And if I had one wish come true. . . I’d surf 'til the sun sets beyond the horizon";
+	while(ros::ok()){
+
+	ros::spinOnce();	
+
+	words.data =" I can work";
+	talk.publish(words);
+	
+	ros::spinOnce();
+	loop_rate.sleep();
+
+	words.data = "Aloha ē, aloha ē ʻAnoʻai ke aloha ē Aloha ē, aloha ē ʻAnoʻai ke aloha ē There’s no place I’d rather be Than on my surfboard out at sea Lingering in the ocean blue And if I had one wish come true I’d surf 'til the sun sets beyond the horizon";
 	talk.publish(words);
 	ros::Duration(10).sleep();
-	
+	ros::shutdown();
+	}	
+
 	return 0;
 }
