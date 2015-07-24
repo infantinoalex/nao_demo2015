c8a64001dee8ba39071d8af374cf2a370a8dff05
Alexander Infantino <infantinoalex@gmail.com>
2015-07-24 13:43:10 -0400

    done

--- programs/functions/src/horror.cpp
+++ programs/functions/src/horror.cpp
@@ -395,9 +395,6 @@ int main(int argc, char **argv) {
         pub_walk.publish(walk);
   
         /************************************************/
-      bool run = true;
-
-      while (run == true) {
        
         /************************************************/
        
@@ -429,7 +426,7 @@ int main(int argc, char **argv) {
         //pub_narration.publish(narration);
   
         lsp.joint_angles[0] = -1.0;
-        lsp.speed = 0.1;
+        lsp.speed = 0.4;
         pub_move.publish(lsp);
   
         lsr.joint_angles[0] = 0.25;
@@ -504,7 +501,7 @@ int main(int argc, char **argv) {
         play.request.file_path.data = "/music/evil_laugh.wav";
         client.call(play);
   
-        ros::Duration(0.5).sleep();
+        ros::Duration(1).sleep();
  
         /************************************************/
    
@@ -519,15 +516,12 @@ int main(int argc, char **argv) {
         hp.speed = 0.3;
         pub_move.publish(hp);
  
-        ros::Duration(0.5).sleep();
+        ros::Duration(1).sleep();
         
         /************************************************/
-  
-        narration.data = "So long.";
-        pub_narration.publish(narration);
-
+  	
         lsp.joint_angles[0] = 1.6;
-        lsp.speed = 0.5;
+        lsp.speed = 0.9;
         pub_move.publish(lsp);
 
         lsr.joint_angles[0] = 0.0;
@@ -542,7 +536,7 @@ int main(int argc, char **argv) {
         ler.speed = 0.5;
         pub_move.publish(ler);
    
-        lwy.joint_angles[0] = 0.0;
+        lwy.joint_angles[0] = -1.5;
         lwy.speed = 0.5;
         pub_move.publish(lwy);
 
@@ -568,11 +562,14 @@ int main(int argc, char **argv) {
         pub_move.publish(rh);
   
         ros::Duration(0.5).sleep();
-
+	
 	play.request.file_path.data = "/music/knife_sound.ogg";
         client.call(play);
-
+	
 	ros::Duration(1).sleep();
+
+	narration.data = "So long.";
+        pub_narration.publish(narration);
 
         /************************************************/
  
@@ -592,12 +589,6 @@ int main(int argc, char **argv) {
         //ros::Duration(2).sleep();
      
         /************************************************/
-
-    }
-
-    ros::spinOnce();
-    loop_rate.sleep();
-
   }
 
 
