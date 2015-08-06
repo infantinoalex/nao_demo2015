a2a64c503565df8738438c31485cd6fcfdee4f50
Victoria <Victoria_Albanese@student.uml.edu>
2015-08-06 11:28:55 -0400

    Commented up print_states node

--- naopositions/nao_set_pose/src/print_states.cpp
+++ naopositions/nao_set_pose/src/print_states.cpp
@@ -1,11 +1,33 @@
-#include <ros/ros.h>
-#include <nao_msgs/JointAnglesWithSpeed.h>
-#include <std_msgs/String.h>
-#include <sensor_msgs/JointState.h>
-#include <sstream>
-#include <custom_msgs/states.h>
+/******************************************************************/
+/*  Author: Victoria Albanese                                     */ 
+/*  Affiliations: University of Massachusetts Lowell Robotics Lab */ 
+/*  Node Name: nao_set_pose.cpp                                   */
+/*  Overview: This node reads in the position of each of the      */
+/*            individual joints and prints them to the terminal   */
+/*            in an organized, readable manner.  Basically,       */
+/*            node performs the same function as the "echo"       */
+/*            function but in a more readable fashion.            */
+/*  Improvements: Right now the program reads in all of the       */
+/*                joint states as 0 for the first few iterations  */
+/*                of the while loop.  Identifying what is         */
+/*                causing this problem, and fixing it would be    */
+/*                considered future work on this program.         */
+/******************************************************************/
 
-custom_msgs::states controlmsgs;
+
+
+/******************************************************************/
+
+// Inclusion of Necessary Header Files
+
+#include <ros/ros.h> // include for ros functionality
+#include <sstream> // include for print to terminal functionality
+#include <nao_msgs/JointAnglesWithSpeed.h> // include for joint movement publisher functionality
+#include <sensor_msgs/JointState.h> // include for joint movement subscriber functionality
+
+/******************************************************************/
+
+// Declaration of Global Variables
 
 float hy_state, hp_state, lsp_state, rsp_state,
 	lsr_state, rsr_state, ley_state, rey_state,
@@ -15,7 +37,14 @@ float hy_state, hp_state, lsp_state, rsp_state,
 	lkp_state, rkp_state, lap_state, rap_state,
 	lar_state, rar_state;
 
-void callback(const sensor_msgs::JointState::ConstPtr& Joints){
+/******************************************************************/
+
+// Callback Functions
+
+// Reads joint state data
+
+void callback(const sensor_msgs::JointState::ConstPtr& Joints) {
+
   	hy_state = Joints->position[0];
   	hp_state = Joints->position[1];
   	lsp_state = Joints->position[2];
@@ -45,26 +74,32 @@ void callback(const sensor_msgs::JointState::ConstPtr& Joints){
 	
 }
 
-void controlcb(const custom_msgs::states States){
-	controlmsgs = States;
-}
+/******************************************************************/
+
+// Main
 
 int main(int argc, char **argv) {
 
+	/******************************************************************/
+
+	// Various Initializations
+
+	// Initializing "set_pose" topic and node
 	ros::init(argc, argv, "set_pose");
   	ros::NodeHandle node;
 
-  	//All the publishers
-  	ros::Publisher pub_narration = node.advertise<std_msgs::String>("speech", 100);
+  	// Initializing all the publisher objects
   	ros::Publisher pub_move = node.advertise<nao_msgs::JointAnglesWithSpeed>("joint_angles", 100);
-  	ros::Publisher pub_contrl = node.advertise<custom_msgs::states>("/control_msgs", 100);
 
-  	//All the subscribers
+  	// Initializing all the subscriber objects
   	ros::Subscriber sub_joints = node.subscribe("joint_states", 100, callback);
-  	ros::Subscriber sub_ctrl = node.subscribe("/control_msgs", 100, controlcb);
 	
-  	//All the message declarations
-  	std_msgs::String narration;
+	/******************************************************************/
+
+	// Various Declarations
+
+  	// All the message declarations
+	// Must be declared before joint publisher is called
   	nao_msgs::JointAnglesWithSpeed hy;
   	nao_msgs::JointAnglesWithSpeed hp;
   	nao_msgs::JointAnglesWithSpeed lsp;
@@ -92,20 +127,8 @@ int main(int argc, char **argv) {
   	nao_msgs::JointAnglesWithSpeed lar;
   	nao_msgs::JointAnglesWithSpeed rar;
 
-  	//All check variable declarations
-  	bool all_good = false, hy_check = false, hp_check = false,
-       		lsp_check = false, rsp_check = false, lsr_check = false,
-       		rsr_check = false, ley_check = false, rey_check = false,
-       		ler_check = false, rer_check = false, lwy_check = false,
-       		rwy_check = false, lh_check = false, rh_check = false,
-       		lhyp_check = false, rhyp_check = false, lhp_check = false,
-       		rhp_check = false, lhr_check = false, rhr_check = false,
-       		lkp_check = false, rkp_check = false, lap_check = false,
-       		rap_check = false, lar_check = false, rar_check = false;
-
-	int i = 0;
-
-  	//All joint name statements
+  	// All the joint name statements
+	// Must be declared before joint publisher is called
   	hy.joint_names.push_back("HeadYaw");
   	hp.joint_names.push_back("HeadPitch");
   	lsp.joint_names.push_back("LShoulderPitch");
@@ -133,7 +156,8 @@ int main(int argc, char **argv) {
   	lar.joint_names.push_back("LAnkleRoll");
   	rar.joint_names.push_back("RAnkleRoll");
 	
-  	//All joint angle statements
+  	// All the joint angle statements
+	// Must be declared before the joint publisher is called
   	hy.joint_angles.push_back(0);
   	hp.joint_angles.push_back(0);
   	lsp.joint_angles.push_back(0);
@@ -161,47 +185,61 @@ int main(int argc, char **argv) {
   	lar.joint_angles.push_back(0);
   	rar.joint_angles.push_back(0);
 
-  	ros::Rate loop_rate(10); 
+	/******************************************************************/
 
+	// Loop rate and while loop are initialized
+
+  	ros::Rate loop_rate(10); 
 	while (ros::ok()) {
 
 		ros::spinOnce();
 		loop_rate.sleep();
 
     		/************************************************/
-    
-  		ROS_INFO("HeadYaw State: [ %.2f ]\t", hy_state);
+   
+		// Joint states are printed to the terminal
+ 
+  		ROS_INFO("HeadYaw State: [ %.2f ]\n", hy_state);
   		ROS_INFO("HeadPitch State: [ %.2f ]\n", hp_state);
-  		ROS_INFO("LShoulderPitch State: [ %.2f ]\t", lsp_state);
-  		ROS_INFO("RShoulderPitch State: [ %.2f ]\r", rsp_state);
-  		ROS_INFO("LShoulderRoll State: [ %.2f ]\t", lsr_state);
-  		ROS_INFO("RShoulderRoll State: [ %.2f ]\r", rsr_state);
-  		ROS_INFO("LElbowYaw State: [ %.2f ]\t", ley_state);
-  		ROS_INFO("RElbowYaw State: [ %.2f ]\r", rey_state);
-  		ROS_INFO("LElbowRoll State: [ %.2f ]\t", ler_state);
-  		ROS_INFO("RElbowRoll State: [ %.2f ]\r", rer_state);
-  		ROS_INFO("LWristYaw State: [ %.2f ]\t", lwy_state);
-  		ROS_INFO("RWristYaw State: [ %.2f ]\r", rwy_state);
-  		ROS_INFO("LHand State: [ %.2f ]\t", lh_state);
-  		ROS_INFO("RHand State: [ %.2f ]\r", rh_state);
-  		ROS_INFO("LHipYawPitch State: [ %.2f ]\t", lhyp_state);
-  		ROS_INFO("RHipYawPitch State: [ %.2f ]\r", rhyp_state);
-  		ROS_INFO("LHipPitch State: [ %.2f ]\t", lhp_state);
-  		ROS_INFO("RHipPitch State: [ %.2f ]\r", rhp_state);
-  		ROS_INFO("LHipRoll State: [ %.2f ]\t", lhr_state);
-  		ROS_INFO("RHipRoll State: [ %.2f ]\r", rhr_state);
-  		ROS_INFO("LKneePitch State: [ %.2f ]\t", lkp_state);
-  		ROS_INFO("RKneePitch State: [ %.2f ]\r", rkp_state);
-  		ROS_INFO("LAnklePitch State: [ %.2f ]\t", lap_state);
-  		ROS_INFO("RAnklePitch State: [ %.2f ]\r", rap_state);
-  		ROS_INFO("LAnkleRoll State: [ %.2f ]\t", lar_state);
-  		ROS_INFO("RAnkleRoll State: [ %.2f ]\r", rar_state);
+  		ROS_INFO("LShoulderPitch State: [ %.2f ]\n", lsp_state);
+  		ROS_INFO("RShoulderPitch State: [ %.2f ]\n", rsp_state);
+  		ROS_INFO("LShoulderRoll State: [ %.2f ]\n", lsr_state);
+  		ROS_INFO("RShoulderRoll State: [ %.2f ]\n", rsr_state);
+  		ROS_INFO("LElbowYaw State: [ %.2f ]\n", ley_state);
+  		ROS_INFO("RElbowYaw State: [ %.2f ]\n", rey_state);
+  		ROS_INFO("LElbowRoll State: [ %.2f ]\n", ler_state);
+  		ROS_INFO("RElbowRoll State: [ %.2f ]\n", rer_state);
+  		ROS_INFO("LWristYaw State: [ %.2f ]\n", lwy_state);
+  		ROS_INFO("RWristYaw State: [ %.2f ]\n", rwy_state);
+  		ROS_INFO("LHand State: [ %.2f ]\n", lh_state);
+  		ROS_INFO("RHand State: [ %.2f ]\n", rh_state);
+  		ROS_INFO("LHipYawPitch State: [ %.2f ]\n", lhyp_state);
+  		ROS_INFO("RHipYawPitch State: [ %.2f ]\n", rhyp_state);
+  		ROS_INFO("LHipPitch State: [ %.2f ]\n", lhp_state);
+  		ROS_INFO("RHipPitch State: [ %.2f ]\n", rhp_state);
+  		ROS_INFO("LHipRoll State: [ %.2f ]\n", lhr_state);
+  		ROS_INFO("RHipRoll State: [ %.2f ]\n", rhr_state);
+  		ROS_INFO("LKneePitch State: [ %.2f ]\n", lkp_state);
+  		ROS_INFO("RKneePitch State: [ %.2f ]\n", rkp_state);
+  		ROS_INFO("LAnklePitch State: [ %.2f ]\n", lap_state);
+  		ROS_INFO("RAnklePitch State: [ %.2f ]\n", rap_state);
+  		ROS_INFO("LAnkleRoll State: [ %.2f ]\n", lar_state);
+  		ROS_INFO("RAnkleRoll State: [ %.2f ]\n", rar_state);
 
   		/************************************************/
 
  	}
 
+	// End of while loop
+
+	/******************************************************************/
+
 
  	return 0;
 
 }
+
+// End of Main
+
+/******************************************************************/
+
